28a3254be5ae47aafabf9e94cba8ff175966d4f6
Liz P <parisielizabeth@gmail.com>
2020-08-26 22:02:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,10 +29,11 @@ class Bird {
   }
   
   speak(){
-    if (this.sex === male)
-    return `It's me! ${this.name}, the parrot!`;
-    
-    else 
+    if (this.sex === male){
+      return `It's me! ${this.name}, the parrot!`;
+    }
+    else{ 
       return `${this.name} says squawk!`;
+    }
   }
 }
